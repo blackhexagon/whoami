12afb7c926e112d48e0699016b4e18dab7f86a31
Matouš Trča <matous.trca@gmail.com>
2026-02-27 17:13:05 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,14 @@
 export default {
   name: "Matouš Trča",
   predicates: [
-    ["full-stack vývojář", ["v TypeScriptu", "PHP", "HTML & CSS", "docker"]],
-    ["uživatel", ["Arch Linuxu", "neovimu", "split keyboardu"]],
+    ["full-stack vývojář", ["TypeScript", "PHP", "HTML & CSS", "docker"]],
+    ["uživatel", ["Linuxu", "neovimu", "split keyboardu", "Anki"]],
     ["hrdý táta", ["dvou dcer"]],
     ["manžel", ["lásky mého života"]],
     ["fanoušek", ["hudby", "filmu"]],
     ["čtenář", ["Respektu", "hacker news", "RSS"]],
-    ["duší", ["cyklista", "běžec", "plavec", "pilates"]],
+    ["tělem", ["cyklista", "běžec", "plavec"]],
+    ["duší", ["buddhista","liberál", "abstinent"],
     ["absolvent", ["Univerzity Karlovy", "SPŠ Tábor"]],
     ["promotér", ["táborských parties"]],
     ["bývalý", ["DJ"]],
