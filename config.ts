6e2b7b29d1a8c60d64e650b622d19318dd929b5c
matous <matous.trca@gmail.com>
2026-02-27 12:06:29 +0100

    Add configuration for prettier and update package.json

---
+++ config.ts
@@ -0,0 +1,15 @@
+export default {
+  name: "Matouš Trča",
+  predicates: [
+    ["full-stack vývojář", ["v TypeScriptu", "PHP", "HTML & CSS", "docker"]],
+    ["uživatel", ["Arch Linuxu", "neovimu", "split keyboardu"]],
+    ["hrdý táta", ["dvou dcer"]],
+    ["manžel", ["lásky mého života"]],
+    ["fanoušek", ["hudby", "filmu"]],
+    ["čtenář", ["Respektu", "hacker news", "RSS"]],
+    ["duší", ["cyklista", "běžec", "plavec", "pilates"]],
+    ["absolvent", ["Univerzity Karlovy", "SPŠ Tábor"]],
+    ["promotér", ["táborských parties"]],
+    ["bývalý", ["DJ"]],
+  ],
+};
